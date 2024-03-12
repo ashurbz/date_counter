de48de0a7f696ee8c4d17e40a25ed40f109df13c
Ashish <rbzashu1@gmail.com>
2024-03-12 13:56:07 +0530

    added step and count functionality

--- src/Body.js
+++ src/Body.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./body.css";
 
 const Body = () => {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const handleMinusStep = () => {
+    setStep(step - 1);
+  };
+  const handlePlusStep = () => {
+    setStep(step + 1);
+  };
+
+  const handleMinusCount = () => {
+    setCount(count - step);
+  };
+  const handlePlusCount = () => {
+    setCount(count + step);
+  };
+
   return (
     <div>
       <div className="container">
         <div className="steps">
-          <button>-</button>
-          <span>0</span>
-          <button>+</button>
+          <button onClick={handleMinusStep}>-</button>
+          <span>{step}</span>
+          <button onClick={handlePlusStep}>+</button>
         </div>
         <div className="count">
-          <button>-</button>
-          <span>0</span>
-          <button>+</button>
+          <button onClick={handleMinusCount}>-</button>
+          <span>{count}</span>
+          <button onClick={handlePlusCount}>+</button>
         </div>
         <div className="date">Date</div>
       </div>
